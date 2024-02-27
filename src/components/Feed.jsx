c017ac25bd4d6dbe70a531147c3cc2bd0e7ed7be
ashishverma2831 <ashishverma510207@gmail.com>
2024-02-27 22:42:15 +0530

    Update video and search API endpoints

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -10,7 +10,7 @@ const Feed = ({ sidebar, category, setCategory}) => {
 
     const [videoList, setVideoList] = useState([]);
     const fetchVideos = async () => {
-        const response = await fetch(`https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&videoCategoryId=${category}&key=${API_KEY}`)
+        const response = await fetch(`https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=75&regionCode=IN&videoCategoryId=${category}&key=${API_KEY}`)
         const data = await response.json()
         // console.log(data)
         setVideoList(data.items)
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,13 +10,15 @@ import { Link } from 'react-router-dom'
 import images from '../assets/images.jpg'
 import { API_KEY } from '../data'
 import Feed from './Feed'
+import { value_converter } from '../data'
+import moment from 'moment'
 
 const Navbar = ({sidebar,setSidebar}) => {
     const searchRef = useRef(null);
     const [searchList, setSearchList] = useState([]);
 
     const fetchSearch = async () => {
-        const res = await fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${searchRef.current.value}&key=${API_KEY}`);
+        const res = await fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&q=${searchRef.current.value}&key=${API_KEY}`);
         const data = await res.json();
         // console.log(data);
         setSearchList(data.items);
@@ -24,6 +26,33 @@ const Navbar = ({sidebar,setSidebar}) => {
     console.log(searchList);
 
     //how to use searchList to display the search results
+    const searchResults = ()=>{
+        return(
+            <section className={sidebar ? 'feed max-w-screen-2xl px-4 py-4 gap-4 bg-gray-100 ml-24' : 'bg-gray-100 feed max-w-screen-2xl ml-56 px-4 py-4 '}>
+            {
+                searchList.map((video, index) => {
+                    return (
+                        <Link to={`/video/${video.snippet.categoryId}/${video.id}`} className='w-full' key={index}>
+                            <div className='flex flex-col'>
+                                <div>
+                                    <img className='rounded-lg w-full' src={video.snippet.thumbnails.medium.url} alt="thumbnail" />
+                                </div>
+                                <div className='flex gap-2 '>
+                                    {/* <div className=' w-[50px] h-[50px] my-4 rounded-full'><img className='rounded-full w-full h-full' src={video.snippet.thumbnails.default.url} alt="thumbnail" /></div> */}
+                                    <div className='w-10/12'>
+                                        <h2 className='mx-1 text-lg font-semibold mt-2'> {video.snippet.title} </h2>
+                                        <p className='mx-1 font-semibold text-gray-500'>{video.snippet.channelTitle}</p>
+                                        {/* <p className='mx-1 text-sm text-gray-500'>{value_converter(video.statistics.viewCount)} Views &bull; {moment(video.snippet.publishedAt).fromNow()} </p> */}
+                                    </div>
+                                </div>
+                            </div>
+                        </Link>
+                    )
+                })
+            }
+        </section>
+        )
+    }
 
     return (
         <>
@@ -45,6 +74,7 @@ const Navbar = ({sidebar,setSidebar}) => {
                     <img className='w-8 rounded-full' src={profile} alt="ptofile" />
                 </div>
             </nav>
+            {searchRef.current!==null?searchResults():''}
         </>
     )
 }
